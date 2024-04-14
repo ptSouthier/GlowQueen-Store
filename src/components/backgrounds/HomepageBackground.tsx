713eee75bbaed573fe3fcbd5b59741ef3446f13d
ptSouthier <patrick.southier@hotmail.com>
2024-04-14 18:44:05 -0300

    refac: removing size options from Image components in order to improve image quality, at HomepageBackground

--- src/components/backgrounds/HomepageBackground.tsx
+++ src/components/backgrounds/HomepageBackground.tsx
@@ -9,14 +9,12 @@ export default function HomepageBackground() {
         <Image
           alt="GlowQueen watermark background"
           src={ logoBackground }
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
       </div>
       <div className='womanPortraitWrap'>
         <Image
           alt="Woman with impeccable skin"
           src={ womanPortrait }
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         />
       </div>
     </div>
