400392d20ab2d3a9a9e28bf2d96e69bd6e3cd742
ptSouthier <patrick.southier@hotmail.com>
2024-04-14 19:54:40 -0300

    build: adding "300" weight value at Poppins font import options at layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 
 const poppins = Poppins({
-  weight: ['400', '500', '700'],
+  weight: ['300', '400', '500', '700'],
   style: ['normal'],
   subsets: ['latin'],
   display: 'swap',
