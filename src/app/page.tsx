10924519346c64e7e6bcef73fb2e6d26d2b01406
ptSouthier <patrick.southier@hotmail.com>
2024-04-14 19:56:33 -0300

    feat: implementing recently created components at Home page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import HomepageBackground from "@/src/components/backgrounds/HomepageBackground";
+import Navbar from "@/src/components/navigation/Navbar";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World!</h1>
+    <main className="flex min-h-screen flex-col items-center">
+      <HomepageBackground />
+      <Navbar />
+      <h1 className="text-[4rem] text-primary font-bold leading-[5rem]">
+        Bring back the beauty
+        <br/>and glow of your skin
+      </h1>
     </main>
   );
 }
