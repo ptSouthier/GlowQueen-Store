1369f581670450b8b4a2131e258aa1f3b3391223
ptSouthier <patrick.southier@hotmail.com>
2024-04-14 23:26:41 -0300

    style: implementing "tertiary" color and different screens size options at tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,16 @@ const config: Config = {
     colors: {
       "primary": "#264065",
       "secondary": "#A9D6CB",
+      "tertiary": "#909090",
       ...colors,
     },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1280px',
+      'xl': '1536px',
+      '2xl': '1914px',
+    }
   },
   plugins: [],
 };
