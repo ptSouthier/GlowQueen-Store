ce2baf9aad6b98bdf6df2c837947388d09559017
ptSouthier <patrick.southier@hotmail.com>
2024-04-14 23:31:21 -0300

    feat: creating the ServicesCards component

---
+++ src/components/infos/ServicesCards.tsx
@@ -0,0 +1,29 @@
+import FaceMassage from "@/src/components/icons/FaceMassage";
+import Lotus from "@/src/components/icons/Lotus";
+import Mortar from "@/src/components/icons/Mortar";
+
+
+export default function ServicesCards() {
+  return (
+    <section className="w-[40.1875rem] h-[17.75rem] top-[5.625rem] left-[10rem] relative bg-white flex gap-[4.5rem] p-8 justify-between">
+        <div className="serviceCard">
+          <FaceMassage />
+          <h2>15+</h2>
+          <p>Special</p>
+          <p>Treatment</p>
+        </div>
+        <div className="serviceCard">
+          <Lotus />
+          <h2>25+</h2>
+          <p>Professional</p>
+          <p>Therapist</p>
+        </div>
+        <div className="serviceCard">
+          <Mortar />
+          <h2>100%</h2>
+          <p>Herbal</p>
+          <p>Cream Formula</p>
+        </div>
+    </section>
+  );
+}
