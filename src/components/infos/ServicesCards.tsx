adbf8345ee3eb6dcb79ffcfb8f05b7d3b18e38ba
ptSouthier <patrick.southier@hotmail.com>
2024-04-23 15:10:11 -0300

    style: implementing responsive classes into some ServicesCards.tsx component elements

--- src/components/infos/ServicesCards.tsx
+++ src/components/infos/ServicesCards.tsx
@@ -5,21 +5,21 @@ import Mortar from "@/src/components/icons/Mortar";
 
 export default function ServicesCards() {
   return (
-    <section className="col-start-2 col-end-6 mt-[5.625rem] w-[40.1875rem] backdrop-blur-[0.5rem] bg-white/[.80] flex p-8 justify-between">
+    <section className="col-start-2 col-end-6 mt-14 xl:mt-[5.15rem] 2xl:mt-[5.625rem] w-[32rem] xl:w-[36rem] 2xl:w-[40rem] backdrop-blur-[0.5rem] bg-white/[.80] flex px-7 xl:px-8 py-8 justify-between">
         <div className="serviceCard">
-          <FaceMassage />
+          <FaceMassage className="iconResponsiveScale" />
           <h2>15+</h2>
           <p>Special</p>
           <p>Treatment</p>
         </div>
         <div className="serviceCard">
-          <Lotus />
+          <Lotus className="iconResponsiveScale" />
           <h2>25+</h2>
           <p>Professional</p>
           <p>Therapist</p>
         </div>
         <div className="serviceCard">
-          <Mortar />
+          <Mortar className="iconResponsiveScale" />
           <h2>100%</h2>
           <p>Herbal</p>
           <p>Cream Formula</p>
